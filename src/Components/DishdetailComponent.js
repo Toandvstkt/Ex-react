4e51a15e45d56965cd11f8c529b1cd1a4d2adc32
Dinh Van Toan <vantoanab99@gmail.com>
2023-06-21 08:33:50 +0700

    LAB 6

--- src/Components/DishdetailComponent.js
+++ src/Components/DishdetailComponent.js
@@ -73,10 +73,24 @@ import { Card, CardImg, CardImgOverlay,  CardText, CardBody,
         }
         return (
             <div className="container">
+            <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>{props.dish.name}</h3>
+                    <hr />
+                </div>                
+            </div>
+            <div className="row">
                 <div className="col-12 col-md-5 m-1">
                     <RenderDish dish={props.dish} />
-                    <RenderComments comments={props.dish.comments} />
                 </div>
+                <div className="col-12 col-md-5 m-1">
+                    <RenderComments comments={props.comments} />
+                </div>
+            </div>
             </div>
         );
     };
--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -6,6 +6,7 @@ import { DISHES } from '../shared/dishes';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import About from './AboutComponent';
 import Contact from './ContactComponent'; 
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
@@ -58,6 +59,7 @@ class Main extends Component {
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
               <Route exact path='/contactus' component={Contact} />
               <Route path='/menu/:dishId' component={DishWithId} />
+              <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
               <Redirect to="/home" />
           </Switch>
         
